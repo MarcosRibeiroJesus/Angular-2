b65fd034ccf8bcd1661bce712da22de0a802fd57
MarcosRibeiroJesus <mrj84@outlook.com>
2017-02-13 17:11:07 -0200

    Melhor Experiência do Usuário
    Melhorando a visualização com BS

---
+++ alurapic/client/app/painel/painel.module.ts
@@ -0,0 +1,11 @@
+import { NgModule } from '@angular/core';
+
+import { PainelComponent } from './painel.component';
+
+@NgModule({
+    exports: [PainelComponent],
+    declarations: [PainelComponent]
+})
+
+export class PainelModule { }
+
