9e99b9c06d2cf98a59191cd20e305771414ed229
MarcosRibeiroJesus <mrj84@outlook.com>
2017-02-19 01:44:10 -0300

    Encapsulando nossa View
    O Angular encapsula além dos comportamentos e  dados, também o CSS de
    cada component.

--- alurapic/client/app/cadastro/cadastro.component.ts
+++ alurapic/client/app/cadastro/cadastro.component.ts
@@ -1,21 +1,30 @@
 import { Component } from '@angular/core';
 import { FotoComponent } from '../foto/foto.component';
 import { Http, Headers } from '@angular/http';
+import { FormGroup, FormBuilder, Validators } from '@angular/forms';
 
 @Component({
     moduleId: module.id,
     selector: 'cadastro',
-    templateUrl: './cadastro.component.html'
+    templateUrl: './cadastro.component.html',
+    styleUrls: ['./cadastro.component.css']
 })
 
 export class CadastroComponent {
 
     foto: FotoComponent = new FotoComponent();
     http: Http;
+    meuForm: FormGroup; 
 
-    constructor(http: Http) {
+    constructor(http: Http, fb: FormBuilder) {
 
         this.http = http;
+        this.meuForm = fb.group({
+            titulo: ['', Validators.compose([Validators.required, Validators.minLength(4)])],
+            url: ['', Validators.required],
+            descricao: ['']
+        })
+
     }
 
     cadastrar(event) {
